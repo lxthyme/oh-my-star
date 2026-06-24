a5a55afeb11dc42ad999c8de451a2ee1e8cda089
lxthyme <lx314333@gmail.com>
2026-06-24 19:10:19 +0800

    添加 TagSelect 组件：标签多选与新建

---
+++ app/components/TagSelect.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Select } from "antd"
+
+export interface TagOption {
+  id: number
+  name: string
+}
+
+interface TagSelectProps {
+  allTags: TagOption[]
+  value: string[]
+  onChange: (tagNames: string[]) => void
+}
+
+export default function TagSelect({ allTags, value, onChange }: TagSelectProps) {
+  return (
+    <Select
+      mode="tags"
+      size="small"
+      style={{ minWidth: 160 }}
+      placeholder="添加标签"
+      value={value}
+      options={allTags.map((tag) => ({ label: tag.name, value: tag.name }))}
+      onChange={(next) => onChange(next as string[])}
+    />
+  )
+}
